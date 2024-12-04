69283b042d5606ef54255f40788acb5d9a91f1b0
Willy Ricardo Wijaya <willy.ricardo@gojek.com>
2024-12-04 14:18:11 +0700

    publish page

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "FE Mount Portrait Lens Hunt",
+        "excerpt":"Introduction Currently I looking for Full-frame E-Mount Lens that could cover my needs for Portrait jobs, this post mark my first journey to writing and act as my personal log. Even though I currently going to use the lens with APS-C sensor size camera body, I plan to have Fullframe...","categories": [],
+        "tags": ["lens","photography"],
+        "url": "/2024/11/13/fe-mount-portrait-lens-hunt.html",
+        "teaser": null
+      }]
