3c1501c5212a2a0b81b771e2527206df4425c6d9
Willy Ricardo Wijaya <willyric17@gmail.com>
2024-12-19 14:31:09 +0700

    publish page

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "Draft - FE Mount Portrait Lens Hunt",
+        "excerpt":"Introduction Currently I looking for Full-frame E-Mount Lens that could cover my needs for Portrait jobs, this post mark my first journey to writing and act as my personal log. Even though I currently going to use the lens with APS-C sensor size camera body, I plan to have Fullframe...","categories": ["photography"],
+        "tags": ["lens","photography"],
+        "url": "/photography/2024/11/13/fe-mount-portrait-lens-hunt.html",
+        "teaser": null
+      },{
+        "title": "Building Hugo Pages for Absolutely Beginner",
+        "excerpt":"Preword I can’t find any themes that supports my photography needs in Jekyll. Therefore, I am going to build a Hugo site specially for photography. I tried this 2 months ago, but it doesn’t work. I can’t find decent tutorial to apply the theme that I want, most of Quick...","categories": ["software"],
+        "tags": ["frontend","golang","hugo"],
+        "url": "/software/2024/12/17/building-hugo-pages-for-absolutely-beginner.html",
+        "teaser": null
+      }]
